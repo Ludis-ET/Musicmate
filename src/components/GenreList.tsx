19e097fa6c669115596313722e28f75af113e395
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-07 15:54:40 +0300

    Refactor: Update layout and styles in App and GenreList components

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,23 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import { useGenres } from "../hooks/useGenres";
+import { getCroppedImage } from "../services/image-url";
 
 export const GenreList = () => {
   const { data } = useGenres();
-  return <div>GenreList</div>;
+  return (
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY='5px'>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImage(genre.image_background)}
+            />
+            <Text>{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
 };
