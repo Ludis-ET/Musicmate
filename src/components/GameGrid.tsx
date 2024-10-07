2b6799fe7bc85291ac93b4bd1c013023f0e5806e
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-07 12:02:02 +0300

    Refactor App.tsx to include GameGrid component

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,25 +1,6 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGames {
-  count: number;
-  results: Game[];
-}
+import { useGames } from "../hooks/useGames";
 
 export const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGames>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
+  const { games, erros } = useGames();
   return <div>GameGrid</div>;
 };
